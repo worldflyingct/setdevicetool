858d36de5b882f6e6f061edf4cbfd98813fe7fbf
jevian ma（马作伟） <jevian_ma@worldflying.cn>
2022-01-05 23:46:59 +0800

    强制指定字体与大小，实现字体统一

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include "mainwindow/mainwindow.h"
 #include <QApplication>
+#include <QTextCodec>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setFont(QFont("simsun", 9, QFont::Normal, false));
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
+
     MainWindow w;
     w.show();
 
